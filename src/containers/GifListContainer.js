d0b5737bf5e1b99df476f55339898d8d976b410c
Kelsey Fecho <kfecho30@gmail.com>
2018-08-06 18:38:40 +0000

    automatically backed up by learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
 
 export default class GifListContainer extends Component{
-  
+  constructor(){
+    super();
+    this.state={
+      gifs: []
+    }
+  }
 }
